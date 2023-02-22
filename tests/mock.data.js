b7d85070e4a4d050765acf249909515f1285aaa9
Charles E. Lehner <cel@celehner.com>
2023-02-22 16:26:10 -0500

    Do not add UUID URN to credential.
    URN is not a URL.

--- tests/mock.data.js
+++ tests/mock.data.js
@@ -3,17 +3,15 @@
  */
 import {createRequire} from 'node:module';
 import {klona} from 'klona';
-import {v4 as uuidv4} from 'uuid';
 const require = createRequire(import.meta.url);
 const validVc = require('./validVc.json');
 
-// copies a validVc and adds an id.
+// copies a validVc
 export const createRequestBody = ({issuer, vc = validVc}) => {
   const {settings: {id, options}} = issuer;
   const credential = klona(vc);
   // convert from millisecond to seconds precision
   credential.issuanceDate = createISOTimeStamp();
-  credential.id = `urn:uuid:${uuidv4()}`;
   credential.issuer = id;
   return {
     credential,
