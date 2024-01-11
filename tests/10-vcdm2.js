694c91076369cbc2e3ddacd9ce80139ba2656cc8
Andrew Jones <ajones@digitalbazaar.com>
2024-01-11 20:52:39 +0000

    Add test for multiple credentialStatus ids.

--- tests/10-vcdm2.js
+++ tests/10-vcdm2.js
@@ -450,6 +450,9 @@ describe('Verifiable Credentials Data Model v2.0', function() {
           './input/credential-status-missing-id-ok.json'));
         await assert.rejects(endpoints.issue(require(
           './input/credential-status-nonurl-id-fail.json')));
+        await assert.rejects(endpoints.issue(require(
+          './input/credential-status-multiple-id-fail.json'
+        )));
       });
       reportRow('In Verifiable Presentations, the verifiableCredential ' +
         'property MAY be present. The value MUST be an array of one or more ' +
