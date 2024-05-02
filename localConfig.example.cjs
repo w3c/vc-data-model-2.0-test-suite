d12b133fd4dda62c7d3b609c2939eb07bd0eead9
Benjamin Young <byoung@digitalbazaar.com>
2024-05-02 13:54:42 -0400

    Explain local testing config in README.

---
+++ localConfig.example.cjs
@@ -0,0 +1,19 @@
+// Rename this file to localConfig.cjs
+// you can specify a BASE_URL before running the tests such as:
+// BASE_URL=http://localhost:40443/zDdfsdfs npm test
+const baseUrl = process.env.BASE_URL || 'https://localhost:40443/id';
+module.exports = {
+  settings: {},
+  implementations: [{
+    name: 'My Company',
+    implementation: 'My Implementation Name',
+    issuers: [{
+      id: 'did:myMethod:implementation:issuer:id',
+      endpoint: `${baseUrl}/credentials/issue`
+    }],
+    verifiers: [{
+      id: 'did:myMethod:implementation:verifier:id',
+      endpoint: `${baseUrl}/credentials/verify`
+    }]
+  }]
+};
