338d44a7cb388def65d59623f962141c85212adc
Benjamin Young <byoung@digitalbazaar.com>
2024-07-11 15:13:07 -0400

    Move Data Schemas tests up from Advanced.
    Update language to match spec.

--- tests/10-vcdm2.js
+++ tests/10-vcdm2.js
@@ -829,6 +829,46 @@ describe('VP - Presentations Including Holder Claims', function() {
   }
 });
 
+// 4.13 Data Schemas https://w3c.github.io/vc-data-model/#data-schemas
+describe('Data Schemas', function() {
+  setupMatrix.call(this);
+  for(const [name, implementation] of match) {
+    const endpoints = new TestEndpoints({implementation, tag});
+
+    describe(name, function() {
+      beforeEach(addPerTestMetadata);
+
+      it('The value of the credentialSchema property MUST be one or more ' +
+        'data schemas that provide verifiers with enough information to ' +
+        'determine whether the provided data conforms to the provided ' +
+        'schema(s).', async function() {
+        this.test.link = `https://w3c.github.io/vc-data-model/#data-schemas:~:text=The%20value%20of%20the%20credentialSchema%20property%20MUST%20be%20one%20or%20more%20data%20schemas%20that%20provide%20verifiers%20with%20enough%20information%20to%20determine%20whether%20the%20provided%20data%20conforms%20to%20the%20provided%20schema(s).`;
+        await endpoints.issue(require('./input/credential-schema-ok.json'));
+        await endpoints.issue(require('./input/credential-schemas-ok.json'));
+      });
+
+      it('Each credentialSchema MUST specify its type (for example, ' +
+        'JsonSchema), and an id property that MUST be a URL identifying the ' +
+        'schema file.', async function() {
+        this.test.link = `https://w3c.github.io/vc-data-model/#data-schemas:~:text=Each%20credentialSchema%20MUST%20specify%20its%20type%20(for%20example%2C%20JsonSchema)%2C%20and%20an%20id%20property%20that%20MUST%20be%20a%20URL%20identifying%20the%20schema%20file.`;
+        await assert.rejects(endpoints.issue(require(
+          './input/credential-schema-no-type-fail.json')));
+        await assert.rejects(endpoints.issue(require(
+          './input/credential-schema-no-id-fail.json')));
+        await assert.rejects(endpoints.issue(require(
+          './input/credential-schema-non-url-id-fail.json')));
+      });
+
+      it('If multiple schemas are present, validity is determined according ' +
+        'to the processing rules outlined by each associated ' +
+        'credentialSchema type property.', async function() {
+        this.test.link = `https://w3c.github.io/vc-data-model/#data-schemas:~:text=If%20multiple%20schemas%20are%20present%2C%20validity%20is%20determined%20according%20to%20the%20processing%20rules%20outlined%20by%20each%20associated%20credentialSchema%20type%20property.`;
+        await endpoints.issue(require('./input/credential-schemas-ok.json'));
+      });
+    });
+  }
+});
+
 // 5. Advanced Concepts https://w3c.github.io/vc-data-model/#advanced-concepts
 describe('Advanced', function() {
   setupMatrix.call(this);
@@ -847,25 +887,6 @@ describe('Advanced', function() {
           './input/credential-redef-type-fail.json')));
         await assert.rejects(endpoints.issue(require(
           './input/credential-redef-type2-fail.json')));
-      });
-      it('The value of the credentialSchema property MUST be one or ' +
-        'more data schemas that provide verifiers with enough information to ' +
-        'determine whether the provided data conforms to the provided ' +
-        'schema(s).', async function() {
-        await endpoints.issue(require('./input/credential-schema-ok.json'));
-        await endpoints.issue(require('./input/credential-schemas-ok.json'));
-      });
-      it('Each credentialSchema MUST specify its type (for example, ' +
-        'JsonSchemaValidator2018), and an id property.', async function() {
-        await assert.rejects(endpoints.issue(require(
-          './input/credential-schema-no-type-fail.json')));
-        await assert.rejects(endpoints.issue(require(
-          './input/credential-schema-no-id-fail.json')));
-      });
-      it('credentialSchema id MUST be a URL identifying the schema ' +
-        'file.', async function() {
-        await assert.rejects(endpoints.issue(require(
-          './input/credential-schema-non-url-id-fail.json')));
       });
       it.skip('The value of the refreshService property MUST be one or more ' +
         'refresh services that provides enough information to the ' +
