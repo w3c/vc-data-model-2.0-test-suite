9e1a7d4c024613a13357b6e24f5067de3ac2031a
Benjamin Young <byoung@digitalbazaar.com>
2024-07-18 17:03:51 -0400

    Add messages to Status section.

--- tests/10-vcdm2.js
+++ tests/10-vcdm2.js
@@ -704,11 +704,14 @@ describe('Status', function() {
         this.test.link = `https://w3c.github.io/vc-data-model/#status:~:text=credential%20status%20object.-,If%20present%2C%20the%20normative%20guidance%20in%20Section%204.3%20Identifiers%20MUST%20be%20followed.,-type`;
         // id is optional
         await assert.doesNotReject(endpoints.issue(require(
-          './input/credential-status-missing-id-ok.json')));
+          './input/credential-status-missing-id-ok.json')),
+        'Failed to accept a VC with `credentialStatus` without an `id`.');
         await assert.rejects(endpoints.issue(require(
-          './input/credential-status-multiple-id-fail.json')));
+          './input/credential-status-multiple-id-fail.json')),
+        'Failed to reject a VC with multiple `credentialStatus.id` values.');
         await assert.rejects(endpoints.issue(require(
-          './input/credential-status-nonurl-id-fail.json')));
+          './input/credential-status-nonurl-id-fail.json')),
+        'Failed to reject a VC with a non-URL `credentialStatus.id`.');
       });
       it('(If a credentialStatus property is present), The type ' +
         'property is REQUIRED. It is used to express the type of status ' +
@@ -716,11 +719,14 @@ describe('Status', function() {
         'guidance in Section 4.4 Types MUST be followed.', async function() {
         this.test.link = `https://w3c.github.io/vc-data-model/#status:~:text=The%20type%20property%20is%20REQUIRED.%20It%20is%20used%20to%20express%20the%20type%20of%20status%20information%20expressed%20by%20the%20object.%20The%20related%20normative%20guidance%20in%20Section%204.4%20Types%20MUST%20be%20followed.`;
         await assert.rejects(endpoints.issue(require(
-          './input/credential-status-missing-type-fail.json')));
+          './input/credential-status-missing-type-fail.json')),
+        'Failed to reject a VC missing `credentialStatus.type`.');
         await assert.rejects(endpoints.issue(require(
-          './input/credential-status-type-nonurl-fail.json')));
+          './input/credential-status-type-nonurl-fail.json')),
+        'Failed to reject a VC with a non-URL `credentialStatus.type`.');
         await assert.doesNotReject(endpoints.issue(require(
-          './input/credential-status-ok.json')));
+          './input/credential-status-ok.json')),
+        'Failed to accept a VC with a valid `credentialStatus`.');
       });
       it.skip('Status schemes MUST NOT be implemented in ways that enable ' +
         'tracking of individuals', async function() {
