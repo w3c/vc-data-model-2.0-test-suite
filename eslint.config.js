b96f4ccdd52f2563bda009b33cb04c3279618e84
Benjamin Young <byoung@digitalbazaar.com>
2025-09-04 10:52:34 -0400

    Update eslint; use latest DB eslint node config.

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2024 Digital Bazaar, Inc.
+ *
+ * SPDX-License-Identifier: LicenseRef-w3c-3-clause-bsd-license-2008 OR LicenseRef-w3c-test-suite-license-2023
+ */
+
+import config from '@digitalbazaar/eslint-config/node-recommended';
+
+export default [
+  ...config
+];
